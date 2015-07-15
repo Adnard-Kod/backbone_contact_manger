17ee907cdd8cf438973aa02e23002794337f131f
Adnard-kod <andra.lally@gmail.com>
2015-07-15 17:37:27 -0500

    add select box for filtering contacts

--- js/views/menu.js
+++ js/views/menu.js
@@ -1,38 +1,52 @@
 $(document).ready(function() {
   var ContactView = Backbone.View.extend({
-  tagName: "article",
-  className: "contact-container",
-  template: $("#contactTemplate").html(),
+    tagName: "article",
+    className: "contact-container",
+    template: $("#contactTemplate").html(),
+    render: function () {
+      var tmpl = _.template(this.template);
+      this.$el.html(tmpl(this.model.toJSON()));
+      return this;
+    }
+  });
 
-  render: function () {
-    var tmpl = _.template(this.template);
-    this.$el.html(tmpl(this.model.toJSON()));
-    return this;
-  }
-});
-
-var DirectoryView = Backbone.View.extend({
-  el: $("#contacts"),
-
-  initialize: function () {
-    this.collection = new Directory(contacts);
-    this.render();
-  },
-
-  render: function () {
-    var that = this;
-    _.each(this.collection.models, function (item, COUNTER) {
-      that.renderContact(item);
-    }, this);
-  },
-
-  renderContact: function (item) {
-      var contactView = new ContactView({
-          model: item
+  var DirectoryView = Backbone.View.extend({
+    el: $("#contacts"),
+    initialize: function () {
+      this.collection = new Directory(contacts);
+      $("#filter").append(this.createSelect());
+      this.render();
+    },
+    render: function () {
+      var that = this;
+      _.each(this.collection.models, function (item) {
+        that.renderContact(item);
+      }, this);
+    },
+    renderContact: function (item) {
+        var contactView = new ContactView({
+            model: item
+        });
+        this.$el.append(contactView.render().el);
+    },
+    getTypes: function () {
+      return _.uniq(this.collection.pluck("type"), false, function(type) {
+        return type.toLowerCase();
+      })
+    },
+    createSelect: function() {
+      select = $("<select>", {
+        html: "<option>ALL</option>"
       });
-      this.$el.append(contactView.render().el);
-  }
-});
+     _.each(this.getTypes(), function (item) {
+       var option = $("<option/>", {
+           value: item.toLowerCase(),
+           text: item.toLowerCase()
+       }).appendTo(select);
+     });
+      return select;
+    }
+  });
 
 var directory = new DirectoryView();
 

a18eac8c493b9f4c8ab88097635dad3d983e56bd
Adnard-kod <andra.lally@gmail.com>
2015-07-15 19:23:57 -0500

    add filter functionality

--- js/views/menu.js
+++ js/views/menu.js
@@ -14,20 +14,26 @@ $(document).ready(function() {
     el: $("#contacts"),
     initialize: function () {
       this.collection = new Directory(contacts);
-      $("#filter").append(this.createSelect());
+      this.$el.find("#filter").append(this.createSelect());
       this.render();
+      this.on("change:filterType", this.filterBytype, this);
+      this.collection.on("reset", this.render, this);
+    },
+    events: {
+      "change #filter select": "setFilter"
     },
     render: function () {
+      $('article').remove()
       var that = this;
       _.each(this.collection.models, function (item) {
         that.renderContact(item);
       }, this);
     },
     renderContact: function (item) {
-        var contactView = new ContactView({
-            model: item
-        });
-        this.$el.append(contactView.render().el);
+      var contactView = new ContactView({
+          model: item
+      });
+      this.$el.append(contactView.render().el);
     },
     getTypes: function () {
       return _.uniq(this.collection.pluck("type"), false, function(type) {
@@ -35,9 +41,10 @@ $(document).ready(function() {
       })
     },
     createSelect: function() {
-      select = $("<select>", {
-        html: "<option>ALL</option>"
+      select = $("<select class='form-control'>", {
+        // html: "<option>ALL</option>" dont know why this is not working
       });
+      $("<option value='all'>ALL</option>").appendTo(select) //quick fix
      _.each(this.getTypes(), function (item) {
        var option = $("<option/>", {
            value: item.toLowerCase(),
@@ -45,6 +52,21 @@ $(document).ready(function() {
        }).appendTo(select);
      });
       return select;
+    },
+    setFilter: function(e) {
+      this.filterType = e.currentTarget.value;
+      this.filterBytype(this.filterType)
+    },
+    filterBytype: function(filter) {
+      if(this.filterType === "all"){
+        this.collection.reset(contacts);
+      } else {
+        this.collection.reset(contacts, {silent: true});
+        var filtered = _.filter(this.collection.models, function(item) {
+          return item.get("type").toLowerCase() === filter;
+        })
+        this.collection.reset(filtered);
+      }
     }
   });
 
